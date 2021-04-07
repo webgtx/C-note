783fe7a7b262887d69f36df21ba70f4e8206d0b3
webgtx <dxv1d@tuta.io>
2021-04-07 14:43:42 +0300

    Added new chapter

---
+++ data-structures/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+struct Hero {
+    char name[12];
+    int lvl;
+    float hp;
+};
+
+int main() {
+    struct Hero player;
+        strcpy(player.name, "Link");
+        player.lvl = 5;
+        player.hp = 100.0f;
+
+    struct Hero npc = {
+        "BIMO",
+        20,
+        200.0f
+    };
+
+    printf("Hero stats\n Name: %s\n LVL: %d\n HP:%.1f\n", player.name, player.lvl, player.hp);
+}
